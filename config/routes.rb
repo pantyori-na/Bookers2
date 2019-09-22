e89cee56a26acd82fb956ef305a9494bcd98bcdc
中村　圭介 <kouzu.keinaka8@gmail.com>
2019-09-22 03:33:11 +0000

    [add1]Bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'books#index'
-  resources :books, only: [:new, :create, :index, :show]
+  resources :books
+  resources :users, only: [:show, :index, :edit, :update]
 end
