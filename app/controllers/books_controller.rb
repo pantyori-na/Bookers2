94dbca1b5aca2ac35a0f71bc995d430339bef276
中村　圭介 <kouzu.keinaka8@gmail.com>
2019-09-22 05:19:59 +0000

    [add2]Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,37 @@
 class BooksController < ApplicationController
-	def new
-		@book = Book.new
-	end
-	def create
-		@book = Book.new(book_params)
-		@book.user_id = current_user.id
-		@book.save
-		redirect_to root_path
-	end
 	def index
-		@book = Book.new
+		@book_new = Book.new
 		@books = Book.all
 	end
 	def show
+		@book_new = Book.new
 		@book = Book.find(params[:id])
+	end
+	def edit
+		@book = Book.find(params[:id])
+	end
+	def create
+		@book = @book_new
+		@book = Book.new(book_params)
+		@book.user_id = current_user.id
+		@book.save
+		if @book.save
+			redirect_to books_path
+		else
+			@book_new = @book
+			@books = Book.all
+			render :index
+		end
+	end
+	def update
+		@book = Book.find(params[:id])
+		@book.update(book_params)
+		redirect_to book_path(@book)
 	end
 	def destroy
 		@book = Book.find(params[:id])
 		@book.destroy
-		redirect_to post_images_path
+		redirect_to books_path
 	end
 	private
 	def book_params
